6dab3c8b9a711aca95c656b5672117c665b5b4bc
jeehwan <tjtojan@naver.com>
2024-06-28 03:32:17 +0900

    Bottom Tab 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./components/shared/Navbar";
+import BottomTab from "./components/shared/BottomTab";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="bg-primary-darken text-bold text-black inline-flex items-center p-4 rounded shadow-md">
         Not Pretty Code 😵
       </div>
+      <BottomTab />
     </div>
   );
 }
---
+++ src/components/shared/BottomTab.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BottomTab() {
+  return (
+    <div className="bg-white shadow-md justify-between flex py-4 px-4 items-center fixed bottom-0 z-50 w-[430px]">
+      <div className="text-xl font-extrabold text-gray-900">Real Auction</div>
+    </div>
+  );
+}
+
+export default BottomTab;
