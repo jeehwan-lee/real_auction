5238bd769ceccf96b201a45e7de7db2211c8e24c
jeehwan <tjtojan@naver.com>
2024-06-28 03:51:06 +0900

    Bottom Tab 컴포넌트 생성

--- src/components/shared/BottomTab.tsx
+++ src/components/shared/BottomTab.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { IoIosSearch } from "react-icons/io";
+import { CiCirclePlus } from "react-icons/ci";
+import { CiBellOn } from "react-icons/ci";
+import { VscAccount } from "react-icons/vsc";
+import { RxHome } from "react-icons/rx";
 
 function BottomTab() {
   return (
     <div className="bg-white shadow-md justify-between flex py-4 px-4 items-center fixed bottom-0 z-50 w-[430px]">
-      <div className="text-xl font-extrabold text-gray-900">Real Auction</div>
+      <RxHome className="text-3xl" />
+      <IoIosSearch className="text-4xl" />
+      <CiCirclePlus className="text-4xl" />
+      <CiBellOn className="text-4xl" />
+      <VscAccount className="text-3xl" />
     </div>
   );
 }
