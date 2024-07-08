727bf301495cd3070f1a81acc87e75a7dd030ca4
jeehwan <tjtojan@naver.com>
2024-07-09 01:25:57 +0900

    Socket 통신 설정

--- src/pages/Auction.tsx
+++ src/pages/Auction.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Flex from "../components/shared/Flex";
 import Text from "../components/shared/Text";
 import LeftMessage from "../components/auction/LeftMessage";
@@ -8,8 +8,37 @@ import Button from "../components/shared/Button";
 import MessageInput from "../components/auction/MessageInput";
 import AuctionInfo from "../components/auction/AuctionInfo";
 import CenterMessage from "../components/auction/CenterMessage";
+import { io } from "socket.io-client";
+import { useParams } from "react-router";
 
 function Auction() {
+  const socket = io(process.env.REACT_APP_BASE_URL, { autoConnect: false });
+
+  const params = useParams();
+
+  useEffect(() => {
+    socket.connect();
+
+    socket.on("connect", () => {
+      console.log("socket connected");
+    });
+
+    socket.on("disconnect", () => {
+      console.log("socket disconnected");
+    });
+
+    socket.on("message", (message) => {
+      console.log("message :", message);
+    });
+
+    socket.emit("join", { userId: 5, roomId: params.id });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("message");
+    };
+  }, []);
   return (
     <>
       <Flex
@@ -33,6 +62,16 @@ function Auction() {
       >
         <MessageInput />
       </Flex>
+      <Button
+        label="전송"
+        onClick={() => {
+          socket.emit("message", {
+            userId: 5,
+            roomId: params.id,
+            message: "hello",
+          });
+        }}
+      ></Button>
     </>
   );
 }
